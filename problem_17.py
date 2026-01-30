2291761ffb546c1a9d8c4821e1f87dcf16d4e99a
francis pravish b <francispravish.b_25ec@kgkite.ac.in>
2026-01-30 15:00:25 +0530

    correct code

--- problem_17.py
+++ problem_17.py
@@ -2,10 +2,6 @@
 # Find and fix the error
 
 def capitalize_words(text):
-    words = text.split()
-    capitalized = []
-    for word in words:
-        capitalized.append(word.capitalize())
     return " ".join(capitalized)
 
 sentence = "hello world from python"
